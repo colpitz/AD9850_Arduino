9b6a9b17b0e40e99d2654763d8b662d1fc1f05f9
colpitz <pedrada@wanadoo.es>
2017-06-01 10:53:55 +0100

    Update Keyboard_4x4.ino

--- Keyboard_4x4.ino
+++ Keyboard_4x4.ino
@@ -1,29 +1,23 @@
-/*  Keypadtest.pde
- *
+/* 
  *  Demonstrate the simplest use of the  keypad library.
  *
- *  The first step is to connect your keypad to the
- *  Arduino  using the pin numbers listed below in
- *  rowPins[] and colPins[]. If you want to use different
- *  pins then  you  can  change  the  numbers below to
- *  match your setup.
- *
+ * Adaptacion del teclado a 4x4 *
  */
 #include <Keypad.h>
 
 const byte ROWS = 4; // Four rows
-const byte COLS = 3; // Three columns
+const byte COLS = 4; // Three columns
 // Define the Keymap
 char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'#','0','*'}
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'#','0','*','D'}
 };
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 9, 8, 7, 6 };
+byte rowPins[ROWS] = { 4, 5, 6, 7 };
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 12, 11, 10 }; 
+byte colPins[COLS] = { 8, 9, 10, 11 }; 
 
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
