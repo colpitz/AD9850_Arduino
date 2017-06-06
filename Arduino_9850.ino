7884ecfdc4e3027f7e018821344d378220d61cbc
colpitz <pedrada@wanadoo.es>
2017-06-06 08:35:41 +0100

    Update Arduino_9850.ino

--- Arduino_9850.ino
+++ Arduino_9850.ino
@@ -16,11 +16,9 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'#','0','*','D'}
 };
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 4, 5, 6, 7 };
-// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 8, 9, 10, 11 }; 
-
+byte rowPins[ROWS] = { 4, 5, 6, 7 }; //pines del arduino
+byte colPins[COLS] = { 8, 9, 10, 11 }; // pines del arduino
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 //AD9850 DDS
 const int W_CLK = A3;
@@ -41,6 +39,22 @@ int buttonPin = 4;
 int state = 0;
 
 void setup() {
+   Serial.begin(9600);
 }
 void loop () {
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+    switch (key)
+    {
+      case '*':
+        digitalWrite(ledpin, LOW);
+        break;
+      case '#':
+        digitalWrite(ledpin, HIGH);
+        break;
+      default:
+        Serial.println(key);
+    }
+  }
 }
